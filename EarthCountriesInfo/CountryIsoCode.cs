031ce076555af33b1b1f961700b0117a459979ca
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-21 22:57:52 +0200

    Update CountryIsoCode.cs

--- EarthCountriesInfo/CountryIsoCode.cs
+++ EarthCountriesInfo/CountryIsoCode.cs
@@ -2,6 +2,231 @@
 {
 	public enum CountryIsoCode
 	{
-		DK
+		CA, // Canada
+		US, // United States
+		BS, // Bahamas
+		BB, // Barbados
+		AI, // Anguilla
+		AG, // Antigua and Barbuda
+		VG, // Virgin Islands (British)
+		VI, // Virgin Islands (U.S.)
+		KY, // Cayman Islands
+		BM, // Bermuda
+		GD, // Grenada
+		TC, // Turks and Caicos Islands
+		MS, // Montserrat
+		GU, // Guam
+		LC, // Saint Lucia
+		DM, // Dominica
+		VC, // Saint Vincent and the Grenadines
+		PR, // Puerto Rico
+		DO, // Dominican Republic
+		TT, // Trinidad and Tobago
+		KN, // Saint Kitts and Nevis
+		JM, // Jamaica
+		KZ, // Kazakhstan
+		RU, // Russian Federation
+		EG, // Egypt
+		ZA, // South Africa
+		GR, // Greece
+		NL, // Netherlands
+		BE, // Belgium
+		FR, // France
+		ES, // Spain
+		HU, // Hungary
+		IT, // Italy
+		RO, // Romania
+		CH, // Switzerland
+		AT, // Austria
+		GB, // United Kingdom
+		DK, // Denmark
+		SE, // Sweden
+		NO, // Norway
+		SJ, // Svalbard and Jan Mayen Islands
+		PL, // Poland
+		DE, // Germany
+		PE, // Peru
+		MX, // Mexico
+		CU, // Cuba
+		AR, // Argentina
+		BR, // Brazil
+		CL, // Chile
+		CO, // Colombia
+		VE, // Venezuela
+		MY, // Malaysia
+		AU, // Australia
+		ID, // Indonesia
+		PH, // Philippines
+		NZ, // New Zealand
+		PN, // Pitcairn
+		SG, // Singapore
+		TH, // Thailand
+		JP, // Japan
+		KR, // Republic of Korea
+		VN, // Vietnam
+		CN, // China
+		TR, // Turkey
+		IN, // Bharat (India)
+		PK, // Pakistan
+		AF, // Afghanistan
+		LK, // Sri Lanka
+		MM, // Myanmar
+		IR, // Iran
+		MA, // Morocco
+		EH, // Western Sahara
+		DZ, // Algeria
+		TN, // Tunisia
+		LY, // Libya
+		GM, // Gambia
+		SN, // Senegal
+		MR, // Mauritania
+		ML, // Mali
+		GN, // Guinea
+		BF, // Burkina Faso
+		NE, // Niger
+		TG, // Togo
+		BJ, // Benin
+		MU, // Mauritius
+		LR, // Liberia
+		SL, // Sierra Leone
+		GH, // Ghana
+		NG, // Nigeria
+		TD, // Chad
+		CF, // Central African Republic
+		CM, // Cameroon
+		CV, // Cape Verde
+		ST, // Sao Tome and Principe
+		GQ, // Equatorial Guinea
+		GA, // Gabon
+		CG, // Congo
+		CD, // Democratic Republic of the Congo
+		ZR, // Zaire (Discontinued)
+		AO, // Angola
+		GW, // Guinea-Bissau
+		SC, // Seychelles
+		SD, // Sudan
+		RW, // Rwanda
+		ET, // Ethiopia
+		SO, // Somalia
+		DJ, // Djibouti
+		KE, // Kenya
+		TZ, // Tanzania
+		UG, // Uganda
+		BI, // Burundi
+		MZ, // Mozambique
+		ZM, // Zambia
+		MG, // Madagascar
+		YT, // Mayotte
+		RE, // Reunion
+		ZW, // Zimbabwe
+		NA, // Namibia
+		MW, // Malawi
+		LS, // Lesotho
+		BW, // Botswana
+		SZ, // Swaziland
+		KM, // Comoros
+		SH, // St. Helena
+		ER, // Eritrea
+		AW, // Aruba
+		FO, // Faroe Islands
+		GL, // Greenland
+		GI, // Gibraltar
+		PT, // Portugal
+		LU, // Luxembourg
+		IE, // Ireland
+		IS, // Iceland
+		AL, // Albania
+		MT, // Malta
+		CY, // Cyprus
+		FI, // Finland
+		BG, // Bulgaria
+		LT, // Lithuania
+		LV, // Latvia
+		EE, // Estonia
+		MD, // Moldova
+		AM, // Armenia
+		BY, // Belarus
+		AD, // Andorra
+		MC, // Monaco
+		SM, // San Marino
+		VA, // Vatican City State
+		UA, // Ukraine
+		YU, // Yugoslavia (Discontinued)
+		HR, // Croatia
+		SI, // Slovenia
+		BA, // Bosnia and Herzegovina
+		MK, // Macedonia
+		CZ, // Czech Republic
+		SK, // Slovakia
+		LI, // Liechtenstein
+		FK, // Falkland Islands
+		BZ, // Belize
+		GT, // Guatemala
+		SV, // El Salvador
+		HN, // Honduras
+		NI, // Nicaragua
+		CR, // Costa Rica
+		PA, // Panama
+		PM, // St. Pierre and Miquelon
+		HT, // Haiti
+		GP, // Guadeloupe
+		BO, // Bolivia
+		GY, // Guyana
+		EC, // Ecuador
+		GF, // French Guiana
+		PY, // Paraguay
+		MQ, // Martinique
+		SR, // Suriname
+		UY, // Uruguay
+		AN, // Netherlands Antilles
+		TL, // East Timor
+		NF, // Norfolk Island
+		BN, // Brunei Darussalam
+		NR, // Nauru
+		PG, // Papua New Guinea
+		TO, // Tonga
+		SB, // Solomon Islands
+		VU, // Vanuatu
+		FJ, // Fiji
+		PW, // Palau
+		WF, // Wallis and Futuna Islands
+		NU, // Niue
+		WS, // Samoa
+		KI, // Kiribati
+		NC, // New Caledonia
+		TV, // Tuvalu
+		PF, // French Polynesia
+		TK, // Tokelau
+		FM, // Micronesia
+		MH, // Marshall Islands
+		KP, // Democratic People's Republic of Korea
+		HK, // Hong Kong
+		MO, // Macao
+		KH, // Cambodia
+		LA, // Lao People's Democratic Republic
+		BD, // Bangladesh
+		TW, // Taiwan
+		MV, // Maldives
+		LB, // Lebanon
+		JO, // Jordan
+		SY, // Syrian Arab Republic
+		IQ, // Iraq
+		KW, // Kuwait
+		SA, // Saudi Arabia
+		YE, // Yemen
+		OM, // Oman
+		AE, // United Arab Emirates
+		IL, // Israel
+		BH, // Bahrain
+		QA, // Qatar
+		BT, // Bhutan
+		MN, // Mongolia
+		NP, // Nepal
+		TJ, // Tajikistan
+		TM, // Turkmenistan
+		AZ, // Azerbaijan
+		GE, // Georgia
+		KG, // Kyrgyzstan
+		UZ // Uzbekistan
 	}
 }
